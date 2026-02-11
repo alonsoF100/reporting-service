21238cf57b2b4acdaa9be6fa9a638d140daf65fd
alonsoF100 <yanul228@mail.ru>
2026-02-11 14:16:29 +0500

    add models

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,3 +1,21 @@
 package models
 
 // TODO добавить модели данных
+type DeviceMessage struct {
+    Number      int    `json:"number"`       // #номер
+    Mqtt        string `json:"mqtt"`         // mqtt
+    Invid       string `json:"invid"`        // инвентарный номер
+    UnitGUID    string `json:"unit_guid"`    // гуид устройства
+    MessageID   string `json:"message_id"`   // id сообщения
+    MessageText string `json:"message_text"` // текст сообщения
+    Context     string `json:"context"`      // среда
+    MessageClass string `json:"message_class"` // класс сообщения [alarm,warning,info,event,comand,waiting,working]
+    Level       int    `json:"level"`        // уровень сообщения [int]
+    Area        string `json:"area"`         // зона переменных HR,IR.I,C
+    Address     string `json:"address"`      // адрес переменной в контроллере
+}
+
+type ParseResult struct {
+	FileName string `json:"file_name"`
+	Messages []DeviceMessage `json:"messages"`
+}
